64c99271d1b78b12075c0fb0dc5b0db084d57c92
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-02 17:08:51 +0100

    create navbar (burger) with router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Books from './components/Books.jsx';
+import Characters from './components/Characters.jsx';
+import Houses from './components/Houses.jsx';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+          <BrowserRouter>
+              <Navbar />
+              <div className="container mt-2" style={{ marginTop: 40 }}>
+                  <Switch>
+                      <Route exact path="/books">
+                          <Books />
+                      </Route>
+                      <Route path="/characters">
+                          <Characters />
+                      </Route>
+                      <Route path="/houses">
+                          <Houses />
+                      </Route>
+                  </Switch>
+              </div>
+          </BrowserRouter>
+
       </header>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,56 +1,71 @@
-import React from 'react';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-import '../../index.css';
-
-class ContentAPI extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: {}
-        };
-    }
-
-    componentDidMount() {
-        fetch("https://www.anapioficeandfire.com/api/")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: result
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
-
-    render() {
-        const { error, isLoaded, items } = this.state;
-        if (error) {
-            return <div>Erreur : {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Chargement…</div>;
-        } else {
-            return (
-                <div>
-                    <p><a href="">Books</a></p>
-                    <p><a href="">Characters</a></p>
-                    <p><a href="">Houses</a></p>
+const Navbar = () => {
+    const [isOpen, setOpen] = useState(false);
+    return (
+        <nav
+            className="navbar is-primary"
+            role="navigation"
+            aria-label="main navigation"
+        >
+            <div className="container">
+                <div className="navbar-brand">
+                    <a
+                        role="button"
+                        className={`navbar-burger burger ${isOpen && "is-active"}`}
+                        aria-label="menu"
+                        aria-expanded="false"
+                        onClick={() => setOpen(!isOpen)}
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
                 </div>
-            );
-        }
-    }
-}
 
+                <div className={`navbar-menu ${isOpen && "is-active"}`}>
+                    <div className="navbar-start">
+                        <NavLink className="navbar-item" activeClassName="is-active" to="/">
+                            Home
+                        </NavLink>
 
-export default ContentAPI;
+                        <NavLink
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            to="/books"
+                        >
+                            Books
+                        </NavLink>
+
+                        <NavLink
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            to="/characters"
+                        >
+                            Characters
+                        </NavLink>
+
+                        <NavLink
+                            className="navbar-item"
+                            activeClassName="is-active"
+                            to="/houses"
+                        >
+                            Houses
+                        </NavLink>
+                    </div>
+
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <a className="button is-white">Log in</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
